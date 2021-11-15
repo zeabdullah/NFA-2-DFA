5a50a9cb8dcaaf4b7da4beac9d987271d8905644
AbdullahZeidan <58212246+AbdullahZeidan@users.noreply.github.com>
2021-11-15 13:33:09 +0200

    - Slightly modified addState method - Started coneptualizing convertToDFA() implementation

--- src/classes/FSA.ts
+++ src/classes/FSA.ts
@@ -41,7 +41,7 @@ interface FSAInterface2 {
    ): Destination;
 
    // The Big Mann
-   convertToDFA(): FSA;
+   convertToDFA(): FSA2;
 }
 
 class FSA2 implements FSAInterface2 {
@@ -62,41 +62,83 @@ class FSA2 implements FSAInterface2 {
       this.alphabet = alphabet;
    }
 
+   // -------- methods ---------
+   //
    addState({ id, isFinal = false, destinations = [] }: State): boolean {
       const newState = { id, isFinal, destinations } as State;
-
-      if (this.states.length === 0) {
-         this.startingStateId = id;
-         this.states = [newState];
-         return true;
-      }
 
       if (this.states.get(id)) {
          alert(`Duplicate state '${id}' already found. new state NOT added.`);
          return false;
       }
+
       this.states.set(id, newState);
+
+      if (this.states.length === 1) {
+         this.startingStateId = id;
+         return true;
+      }
 
       if (newState.isFinal) {
          // push new state to this.finalStates
          this.finalStates = [...this.finalStates, newState.id];
       }
 
+      // check if any of the target destinations are new,
+      // so that we add it to the alphabet
       newState.destinations.forEach(destination => {
          if (!this.alphabet.get(destination.input)) {
             this.alphabet.set(destination.input, destination.input);
          }
       });
-      // this.alphabet.forEach((input) => {
-      //    newState.destinations.forEach((destination) => {
-      //       if (input !== destination.input) {
-      //          this.alphabet = [...this.alphabet, input];
-      //          return;
-      //       }
-      //    });
-      // });
 
       return true;
+   }
+
+   findState(id: string): State | undefined {
+      return this.states.get(id);
+   }
+
+   addDestinationToState(stateId: string, newDestination: Destination): boolean {
+      // if the state we want to mutate doesn't exist, don't do anything.
+      if (!this.findState(stateId)) return false;
+
+      const { input, id: targetId } = newDestination;
+
+      const chosenState = this.findState(targetId);
+
+      // if chosen state doesn't exist, create new state with the targetId
+      if (!chosenState) return this.addState(new State(targetId)); // will always return true
+
+      const alreadyExists = chosenState.destinations.find(
+         currDestination =>
+            currDestination.input === input && currDestination.id === targetId
+      );
+
+      if (!alreadyExists) {
+         chosenState.destinations = [...chosenState.destinations, newDestination];
+         return true;
+      }
+
+      alert(
+         `Duplicate destination [${input}, ${targetId}] found. new destination NOT added.`
+      );
+
+      return false;
+   }
+
+   // converts NFA to DFA
+   convertToDFA(): FSA2 {
+      // - create new FSA, modifying only the new one
+      // const dfa = new FSA2();
+      // const oldStartingState: State = this.states.get(this.startingStateId);
+      // dfa.addState({ oldStartingState.id, false})
+      // - merge destinations of same input to a single object
+      // -
+      // ...
+      // - return it
+      // NOTE: i would either reassign my nfa to a dfa, or create a new variable referencing the new dfa
+      // TODO: implementation...
    }
 }
 
