924653d8960c3c3d671fc4f420805bb5ac8fb51e
Abdullah Zeidan <58212246+AbdullahZeidan@users.noreply.github.com>
2021-11-25 19:07:20 +0200

    - app.ts: Changed to have the code to manipulate the DOM - index.html: wrapped table with form

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
-// Imports
-import FSA from './classes/FSA';
-import State from './classes/State';
-
 // HTML elements
 // const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
-const myNfa = new FSA();
-myNfa.addState(
-   new State('q1', false, [
-      { input: '0', targetId: 'q1' },
-      { input: '0', targetId: 'q2' },
-      { input: '1', targetId: 'q1' }
-   ])
-);
-myNfa.addState(new State('q2', false, [{ input: '1', targetId: 'q3' }]));
-myNfa.addState(new State('q3', true));
 
-console.log(myNfa);
-const dfa = myNfa.convertToDFA();
-console.log(dfa);
+const tableDataRows = document.querySelectorAll<HTMLTableRowElement>('.data-row');
+tableDataRows.forEach(dataRow => {
+   dataRow.addEventListener('click', (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+
+      if (target.matches('i.delete')) {
+         deleteDataRow(dataRow);
+      } else if (target.matches('i.edit')) {
+         editDataRow(dataRow);
+      }
+   });
+});
+
+function deleteDataRow(dataRow: HTMLTableRowElement) {
+   dataRow.remove();
+}
+function editDataRow(dataRow: HTMLTableRowElement) {
+   for (let i = 0; i < dataRow['children'].length; i++) {
+      dataRow['children'][i].toggleAttribute('contenteditable');
+   }
+}
